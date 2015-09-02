364ba7aaf4a7a236cdb458e8a334043138398f5e
amosclerizier <amosclerizier@gmail.com>
2015-09-02 06:56:01 -0400

    Update lab02.py

--- lab02.py
+++ lab02.py
@@ -1,7 +1,8 @@
 # COT 4930  Python Programming
-# name:
-# id  :
+# name: 
+# id  : 
 # lab : 02
+
 
 def fib(n):
 	listholder = []
@@ -9,25 +10,21 @@ def fib(n):
 	if n < 1 :
 		print("Number has to be greater than 0!")
 	elif n == 1 :
-		print ("0")
+		listholder.append(0)
 	elif n == 2:
-		print ("0    1")
+		listholder.append(0)
+		listholder.append(1)
 	else:
 		listholder.append(0)
 		listholder.append(1)
-		for i in range(n-1):
+		for i in range(n-2):
 			a,b = b,a+b
 			listholder.append(a)
 		
-		#for i in range(0, len(listholder), 5):
-			#dontuncomment#print(listholder[i:i + 5])
-			#s = "".join(str(listholder[i:i + 5])).replace(",", "     ").replace("[","").replace("]","")
-			#print (s)
-			
-		for a,b,c,d,e in zip(listholder[::5],listholder[1::5],listholder[2::5],listholder[3::5],listholder[4::5]):
-			print ('{:<20}{:<20}{:<20}{:<20}{:<}'.format(a,b,c,d,e))
-		return a
-	
+	for i in range(0, len(listholder), 6):
+		s = "".join(str(listholder[i:i + 6])).replace(",", "     ").replace("[","").replace("]","")
+		print (s)
+
+
 number = input('What is your number? ')
 fib(int(number))
-#fib(20)
